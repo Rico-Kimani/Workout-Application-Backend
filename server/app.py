3e7ca3dd54a2a0a23693af9bcd759652d5b3e2fa
Rico-Kimani <kimanierick54@gmail.com>
2026-04-16 01:24:23 +0300

    API endpoints

--- server/app.py
+++ server/app.py
@@ -12,5 +12,50 @@ migrate = Migrate(app, db)
 
 # Routes will go here later
 
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    return make_response(jsonify([]), 200)
+
+
+@app.route('/workouts/<int:id>', methods=['GET'])
+def get_workout(id):
+    return make_response(jsonify({}), 200)
+
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+    return make_response(jsonify({}), 201)
+
+
+@app.route('/workouts/<int:id>', methods=['DELETE'])
+def delete_workout(id):
+    return make_response(jsonify({}), 204)
+
+
+
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+    return make_response(jsonify([]), 200)
+
+
+@app.route('/exercises/<int:id>', methods=['GET'])
+def get_exercise(id):
+    return make_response(jsonify({}), 200)
+
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    return make_response(jsonify({}), 201)
+
+
+@app.route('/exercises/<int:id>', methods=['DELETE'])
+def delete_exercise(id):
+    return make_response(jsonify({}), 204)
+
+
+@app.route('/workouts/<int:workout_id>/exercises/<int:exercise_id>/workout_exercises', methods=['POST'])
+def add_exercise_to_workout(workout_id, exercise_id):
+    return make_response(jsonify({}), 201)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
